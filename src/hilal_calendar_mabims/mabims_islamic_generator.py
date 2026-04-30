fa428496b0a22b2c0c7259cac5011d582e5ef1f9
AliCoder8 <215129849+AliCoder8@users.noreply.github.com>
2026-04-30 00:13:01 +0000

    Update mabims_islamic_generator.py

--- src/hilal_calendar_mabims/mabims_islamic_generator.py
+++ src/hilal_calendar_mabims/mabims_islamic_generator.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2026 AliCoder8
 # Licensed under the MIT License.
 # See LICENSE file in the project root for full license information.
-# Website: https://github.com/AliCoder8/hilal-calendar-mabims
+# Website: https://github.com/AliCoder8/pymabims
 
 import math
 from datetime import timedelta
